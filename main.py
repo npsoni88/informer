fe9c4a96ab9942dd22eb35f989636d52ccec04fb
Nishant Soni <nishant@npsoni.com>
2019-08-14 11:41:07 +0530

    operating system output

--- main.py
+++ main.py
@@ -4,13 +4,17 @@ import sys
 def main():
     # read /cat/os-release for linux
     # a simple sys.platform for windows
+    os_name_full = ""
     if 'win32' in sys.platform:
         print("Your have a Windows operating system and that's all I know for now.")
 
     f = open("/etc/os-release", "r")
     if f.mode == "r":
         contents = f.readlines()
-        print(contents[0].strip())
+        for line in contents:
+            if "PRETTY_NAME" in line:
+                os_name_full = line
+        print("Your Operating System is: " + os_name_full.split("=")[1])
 
 
 if __name__ == "__main__":
